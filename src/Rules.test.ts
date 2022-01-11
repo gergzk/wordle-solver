8d931243b6e9f59820897f577468809779d04fa6
Gergely Kota <gergelyk@gmail.com>
2022-01-10 23:48:59 -0800

    Notes

--- src/Rules.test.ts
+++ src/Rules.test.ts
@@ -86,39 +86,43 @@ describe("Rules", () => {
                 })
             });
         });
-        describe("statics", () => {
-            describe("mergeArrays", () => {
-                test("no overlap", () => {
-                    const a1 = "hat".split("");
-                    const a2 = "box".split("");
-                    expect(Rules.mergeArrays(a1, a2)).toStrictEqual("hatbox".split(""));
-                });
-                test("some overlap", () => {
-                    const a1 = "hat".split("");
-                    const a2 = "bot".split("");
-                    expect(Rules.mergeArrays(a1, a2)).toStrictEqual("hatbo".split(""));
-                });
-                test("all overlap", () => {
-                    const a1 = "hat".split("");
-                    const a2 = "hat".split("");
-                    expect(Rules.mergeArrays(a1, a2)).toStrictEqual("hat".split(""));
-                });
+    });
+    describe("merge", () => {
+        test.skip("Put merge tests here", () => {});
+        test.skip("Swap boggle stuff for wordle stuff - session with updating rules", () => {});
+    });
+    describe("statics", () => {
+        describe("mergeArrays", () => {
+            test("no overlap", () => {
+                const a1 = "hat".split("");
+                const a2 = "box".split("");
+                expect(Rules.mergeArrays(a1, a2)).toStrictEqual("hatbox".split(""));
             });
-            describe("placesContains", () => {
-                const places = [{ index: 1, char: "g" }, { index: 3, char: "x" }];
-                test("contains", () => {
-                    const p = { index: 3, char: "x" };
-                    expect(Rules.placesContains(p, places)).toBe(true);
-                });
-                test("not contains", () => {
-                    const p = { index: 2, char: "a" };
-                    expect(Rules.placesContains(p, places)).toBe(false);
-                });
-                test("collision with mismatching char throws", () => {
-                    const p = { index: 3, char: "a" };
-                    expect(() => Rules.placesContains(p, places)).toThrowError();
-                })
+            test("some overlap", () => {
+                const a1 = "hat".split("");
+                const a2 = "bot".split("");
+                expect(Rules.mergeArrays(a1, a2)).toStrictEqual("hatbo".split(""));
             });
+            test("all overlap", () => {
+                const a1 = "hat".split("");
+                const a2 = "hat".split("");
+                expect(Rules.mergeArrays(a1, a2)).toStrictEqual("hat".split(""));
+            });
+        });
+        describe("placesContains", () => {
+            const places = [{ index: 1, char: "g" }, { index: 3, char: "x" }];
+            test("contains", () => {
+                const p = { index: 3, char: "x" };
+                expect(Rules.placesContains(p, places)).toBe(true);
+            });
+            test("not contains", () => {
+                const p = { index: 2, char: "a" };
+                expect(Rules.placesContains(p, places)).toBe(false);
+            });
+            test("collision with mismatching char throws", () => {
+                const p = { index: 3, char: "a" };
+                expect(() => Rules.placesContains(p, places)).toThrowError();
+            })
         });
     });
 });
