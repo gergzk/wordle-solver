5997495be45309b16ade90253d45edd53e0d426a
Gergely Kota <gergelyk@gmail.com>
2022-01-17 15:42:57 -0800

    Update match to use Rule

--- wordleMatches.ts
+++ wordleMatches.ts
@@ -1,15 +1,27 @@
 import { getLegalWords } from "./Words";
-import { Rules } from "./src/Rules";
+import { Rule } from "./src/Rule";
 
 const contains = process.argv[2]?.split("") || [];
 const notContains = process.argv[3]?.split("") || [];
 const placesAsString = process.argv[4] || ".....";
+const notAtAsString = process.argv[5] || "";
 const words = getLegalWords();
 console.log(`Loaded ${words.length} words`);
-//console.log(`Read args ${contains}, ${notContains}, ${placesAsString}`);
 
-const places = placesAsString.split("").map((c,i) => { return { index: i, char: c }}).filter(e => e.char != ".");
-const rules = new Rules(5, contains, notContains, places);
-const matches = words.filter(word => rules.matches(word));
+const rule = new Rule();
+contains.forEach(c => rule.addContains(c));
+notContains.forEach(c => rule.remove(c));
+placesAsString.split("").forEach((c,i) => { 
+    if (c !== ".") {
+        rule.setPosition(i, c);
+    }
+});
+notAtAsString.split(",").forEach(pair => {
+    const ps = pair.split(":");
+    const c = ps[0];
+    const i = Number.parseInt(ps[1]);
+    rule.removeFromPosition(i, c);
+});
+const matches = words.filter(word => rule.matches(word));
 matches.sort();
 console.log(`Matches ${matches.length} words: ${matches.join(", ")}`);
