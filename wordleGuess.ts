a2dfb1a140e02410f4bc1fc87dc561525dd9acc6
Gergely Kota <gergelyk@gmail.com>
2022-01-17 20:12:40 -0800

    Update guess to use Rule

--- wordleGuess.ts
+++ wordleGuess.ts
@@ -1,10 +1,10 @@
 import { getLegalWords } from "./Words";
-import { Rules } from "./src/Rules";
+import { Rule } from "./src/Rule";
 
 const words = getLegalWords();
 console.log(`Loaded ${words.length} words`);
 const goal = process.argv[3] || words[Math.floor(Math.random() * words.length)];
-const rules = Rules.create(process.argv[2], goal);
+const rules = Rule.create(process.argv[2], goal);
 //console.log(`Goal word is ${goal}`);
 const matches = words.filter(word => rules.matches(word));
 console.log(`Matches ${matches.length} words: ${matches.join(", ")}`);
